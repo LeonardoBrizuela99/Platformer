8bab870bd6a5f3a39632564d6f1eb528505fdcf9
leo <leo99brizuela@gmail.com>
2024-05-19 22:59:20 -0300

    Add camera cinemachine

--- Platformer/Assets/Scripts/Player Controller.cs
+++ Platformer/Assets/Scripts/Player Controller.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
     public float gravityScale = 5f;
+    public float rotateSpeed=5f;
 
     private Vector3 moveDirection;
 
     public CharacterController characterController;
-
+    public Camera playerCamara;
+    public GameObject playerModel;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +26,8 @@ public class PlayerController : MonoBehaviour
     {
         float yStore = moveDirection.y;
 
-        moveDirection = new Vector3 (Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
+        //moveDirection = new Vector3 (Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical"))+transform.right*Input.GetAxisRaw("Horizontal");
         moveDirection = moveDirection * moveSpeed;
         moveDirection.y = yStore;
          
@@ -35,5 +39,14 @@ public class PlayerController : MonoBehaviour
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
 
         characterController.Move(moveDirection * Time.deltaTime);
+
+        if(Input.GetAxisRaw("Horizontal")!=0|| Input.GetAxisRaw("Vertical")!=0)
+        {
+            transform.rotation = Quaternion.Euler(0f, playerCamara.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed*Time.deltaTime);
+        }
+
+        
     }
 }
